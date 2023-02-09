cbc09a21fd5f0bfb0044e16ee85cab9ec8cacf4a
DylanDempsey1 <drdempsey18@gmail.com>
2023-02-09 02:30:03 +0000

    ops-challenge-03

--- ops-challenge-03.sh
+++ ops-challenge-03.sh
@@ -7,15 +7,6 @@
 
 # Main
 
-print_history()
-local dylan=$1
-local history_file="$HOME/.bash_history
-
-if [ -e "$history_file" ]; then
-echo "Login history for dylan '$user':"
-cat "$history_file"
-else
-echo "no login history found for user '$dylan'."
-fi
+last -n 200 
 
 #End
