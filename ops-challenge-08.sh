705c1c2ed079a7ff08253d04640d7e7dc8e43013
Dylan Dempsey <119841816+DylanDempsey1@users.noreply.github.com>
2023-02-15 23:37:12 -0800

    Create ops-challenge-08.sh

---
+++ ops-challenge-08.sh
@@ -0,0 +1,11 @@
+#!bin/bash/
+
+# Ops 201 Code Challenge 08
+# Dylan Dempsey
+# 02/15/23
+
+# Main
+
+robocopy C:\Users\j.thompson\Desktop\"My Project Files" C:\Users\j.thompson\My Program Files /s
+
+# End
