0cb55571448ead493e7c53dd09d10de95b0b7fb5
Dylan Dempsey <119841816+DylanDempsey1@users.noreply.github.com>
2023-02-15 23:38:45 -0800

    Update ops-challenge-08.sh

--- ops-challenge-08.sh
+++ ops-challenge-08.sh
@@ -3,7 +3,7 @@
 # Ops 201 Code Challenge 08
 # Dylan Dempsey
 # 02/15/23
-
+# Create a copy of "My Project Files" and pastes into new file to aviod having to do this manually.
 # Main
 
 robocopy C:\Users\j.thompson\Desktop\"My Project Files" C:\Users\j.thompson\My Program Files /s
